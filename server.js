c95f6b25706ee873f95e6ec9aee2ca9954a3adb1
roshan <t3karo22@students.oamk.fi>
2026-05-24 23:44:07 +0300

    server.js

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use('/api/reviews', require('./src/routes/reviews'));
 // Health check
 app.get('/', (req, res) => res.json({ message: 'Neighbor API running 🚀' }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
