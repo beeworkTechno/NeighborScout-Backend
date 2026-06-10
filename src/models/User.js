e3fe0f30805dba94880a3f3cd95841d4e4b5cf4f
roshan <t3karo02@students.oamk.fi>
2026-06-10 14:11:34 +0300

    pre-hook fix

--- src/models/User.js
+++ src/models/User.js
@@ -54,19 +54,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  try {
-    if (!this.isModified('password') || !this.password) {
-      return next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-
-    return next();
-  } catch (error) {
-    return next(error);
+userSchema.pre('save', async function () {
+  if (!this.isModified('password') || !this.password) {
+    return;
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
